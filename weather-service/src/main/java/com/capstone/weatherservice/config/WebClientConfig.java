7e6e8b85d4b51cc085abd23e674d42577e67b0ea
Pao.Vang <vangg.ppao@gmail.com>
2026-02-03 13:13:47 -0600

    laptop

--- weather-service/src/main/java/com/capstone/weatherservice/config/WebClientConfig.java
+++ weather-service/src/main/java/com/capstone/weatherservice/config/WebClientConfig.java
@@ -2,18 +2,15 @@ package com.capstone.weatherservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
-    @Primary
-    public WebClient weatherWebClient() {
+    public WebClient.Builder weatherWebClient() {
         return WebClient.builder()
-                .baseUrl("https://api.openweathermap.org")
-                .build();
+                .baseUrl("https://api.openweathermap.org");
     }
 }
 
