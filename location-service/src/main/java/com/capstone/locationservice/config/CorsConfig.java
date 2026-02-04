57988ff2a851bb923970a4c389a19815f4bba40c
Pao.Vang <vangg.ppao@gmail.com>
2026-02-04 15:48:42 -0600

    laptop

--- location-service/src/main/java/com/capstone/locationservice/config/CorsConfig.java
+++ location-service/src/main/java/com/capstone/locationservice/config/CorsConfig.java
@@ -10,8 +10,9 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
-                .allowedMethods("*")
-                .allowedHeaders("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("Content-Type", "Authorization")
+                .exposedHeaders("Location", "Authorization")
                 .allowCredentials(true);
     }
 }
